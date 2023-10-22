f8936324eccc6515a844d28459e5c5d348b9838d
iqranaz240 <iqranaz240@gmail.com>
2023-10-21 23:46:54 -0700

    initial commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,14 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCqfdF2thW5atrQaW5wbGVpNKpD4RehSW0",
-  authDomain: "smit-b2.firebaseapp.com",
-  databaseURL: "https://smit-b2-default-rtdb.firebaseio.com",
-  projectId: "smit-b2",
-  storageBucket: "smit-b2.appspot.com",
-  messagingSenderId: "688903714025",
-  appId: "1:688903714025:web:095c9199c9fa31a463ac78",
-  measurementId: "G-MGBWHNMVQG"
+  
 };
 
 // Initialize Firebase
